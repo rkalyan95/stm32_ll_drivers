cfdc3db66c1695a365900380635f570e11c99f7c
rkalyan95 <rajatkalyan95@gmail.com>
2025-06-26 12:16:20 +0530

    skeleton for the drivers implementations

--- stm32_ll_rcc/src/stm32l433_ll_rcc.c
+++ stm32_ll_rcc/src/stm32l433_ll_rcc.c
@@ -5,17 +5,21 @@ RCC_TypeDef *RCC_ClockConfigs = ((RCC_TypeDef *) RCC_BASE);
 
 void Rcc_Init(void)
 {
-
-         
+    /*Check the cfg.c file for the settings , to change and use*/
+    
+        uint32_t RegisteTempValue = 0;  
+        RCC_AHB2ENR_Reg_t Rcc_Ahb2Enr_local;  
+        /*Copy the configurations to Actual Hardware Registers*/    
         RCC_ClockConfigs->AHB2ENR = RCC_AHB2ENR_Reg.U;
         RCC_ClockConfigs->CFGR = RCC_CFGR_Reg.U;
         RCC_ClockConfigs->BDCR = RCC_BDCR_Reg.U;
-
- 
-        /*while(!(Rcc_Register_Configs->RCC_AHB2ENR_Reg.B.GPIOA_EN))
+        
+        /* Keep on reading the hardware registers to a local value and operate upon bits for easier access */
+        while(Rcc_Ahb2Enr_local.B.GPIOA_EN != 1)
         {
-            // Wait for GPIOA and GPIOB to be enabled
-        }*/
+            RegisteTempValue = RCC_ClockConfigs->AHB2ENR;
+            Rcc_Ahb2Enr_local.U = RegisteTempValue;
+        }
 
 
 }
