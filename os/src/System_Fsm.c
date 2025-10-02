047e8268f1b96e22f5142cf0bd3defd7bed732f6
rkalyan95 <rajatkalyan95@gmail.com>
2025-10-02 20:16:39 +0530

    pushing minor fixed

--- os/src/System_Fsm.c
+++ os/src/System_Fsm.c
@@ -26,6 +26,19 @@ uint8_t dc;
 
 float dutycurrent=0.0f;
 float dutysaved=0.0f;
+
+const Fsm_t myLedSm[] = {
+    {.current_state=LED_OFF,.evt=BTN_PRESSED,.next_state=LED_ON,.Dispatch=LedOn},
+    {.current_state=LED_OFF,.evt=BTN_RELEASED,.next_state=LED_OFF,.Dispatch=LedOff},
+    {.current_state=LED_ON,.evt=BTN_PRESSED,.next_state=LED_ON,.Dispatch=LedOn},
+    {.current_state=LED_ON,.evt=BTN_RELEASED,.next_state=LED_OFF,.Dispatch=LedOff}
+
+    };
+
+
+
+
+
 uartfsm_t uartfsm[] = 
 {
   {.prefixcmd = 0,.cmd = "stm --version\r", .response = "version 1.0\r\n",.function = sendresponse,.cb = NULL},
@@ -37,97 +50,11 @@ uartfsm_t uartfsm[] =
 };
 
 #define commanddbsize (sizeof(uartfsm)/sizeof(uartfsm[0]))
-
-
-void __isr_exti15_10(void)
-{
-    
-    btn_level = Gpio_GetPin(button_pin.port,button_pin.pin);
-
-    if(btn_level == HIGH)
-    {
-        btn_event = BTN_PRESSED;
-    }
-    else if(btn_level==LOW)
-    {
-        btn_event = BTN_RELEASED;
-    }
-
-    ClearPendingInterrupt(button_pin.pin);
-   
-}
-
-btn_evt_t handledebouncecrudeway(void)
-{
-    fsmEvt = BTN_NONE;
-    debouncetimer++;
-
-    if (btn_event != BTN_NONE && debouncetimer >= DEBOUNCE_TIMER)
-    {
-        uint8_t finalBtnLvl = Gpio_GetPin(button_pin.port, button_pin.pin);
-
-        fsmEvt = (finalBtnLvl == HIGH) ? BTN_PRESSED : BTN_RELEASED;
-
-        debouncetimer = 0;
-        btn_event = BTN_NONE;
-    }
-
-    return fsmEvt;
-}
-
-
-void LedOn(void)
-{
-    Gpio_SetLevel(led_pin.port,led_pin.pin,HIGH);
-
-}
-
-void LedOff(void)
-{
-    Gpio_SetLevel(led_pin.port,led_pin.pin,LOW);
-
-}
-
-const Fsm_t myLedSm[] = {
-    {.current_state=LED_OFF,.evt=BTN_PRESSED,.next_state=LED_ON,.Dispatch=LedOn},
-    {.current_state=LED_OFF,.evt=BTN_RELEASED,.next_state=LED_OFF,.Dispatch=LedOff},
-    {.current_state=LED_ON,.evt=BTN_PRESSED,.next_state=LED_ON,.Dispatch=LedOn},
-    {.current_state=LED_ON,.evt=BTN_RELEASED,.next_state=LED_OFF,.Dispatch=LedOff}
-
-    };
-
-
 #define FS_CNT    (sizeof(myLedSm)/sizeof(myLedSm[0]))
-
 
 static void Fsm_Init(void)
 {
     LedOff();
-}
-
-led_states_t Fsm_Run(btn_evt_t evt , led_states_t currentstate)
-{
-    led_states_t nextState = currentstate;
-    for(uint32_t i=0;i<FS_CNT;i++)
-    {
-        if(evt==myLedSm[i].evt && currentstate == myLedSm[i].current_state)   /* Match Found */
-        {
-            myLedSm[i].Dispatch();
-            nextState = myLedSm[i].next_state;
-            break;
-        }
-    }
-
-    return nextState;
-
-}
-
-void LedBtnFsm_Run(void)
-{
-  finalBtnEvt = BTN_NONE;
-  finalBtnEvt = handledebouncecrudeway();
-        
-  led_state_main = Fsm_Run(finalBtnEvt,led_state_main);
 }
 
 
@@ -250,39 +177,6 @@ static float basic_atof(const char *s)
     return sign * result;
 }
 
-
-void servoanglerxed(uint8_t *param)
-{
-  uint8_t str[256];
-  float anglef = 0.0f;
-  float dutycycle = 0.0f;
-  anglef = basic_atof((const char *)param);
-  TurnServo(anglef,2);
-  printdebugstring("Updated Servo\r\n");
-}
-
-void servofeedback(uint8_t *param)
-{
-
-    dc = (uint8_t)(dutysaved*100.0f);
-    //printdebugstring("Servo Feedback Rxed\r\n");
-    Uart_ll_SendByte(dc+0x30);
-    Uart_ll_SendByte(0x0D);
-
-}
-
-
-
-void setledon(uint8_t *param)
-{
-  LedOn();
-}
-
-void setledoff(uint8_t *param)
-{
-  LedOff();
-}
-
 static int is_prefix_of(const char *haystack, const char *needle,uint8_t *outstr)
 {
     uint8_t idx = 0;
@@ -313,6 +207,98 @@ static int is_prefix_of(const char *haystack, const char *needle,uint8_t *outstr
 }
 
 
+
+
+btn_evt_t handledebouncecrudeway(void)
+{
+    fsmEvt = BTN_NONE;
+    debouncetimer++;
+
+    if (btn_event != BTN_NONE && debouncetimer >= DEBOUNCE_TIMER)
+    {
+        uint8_t finalBtnLvl = Gpio_GetPin(button_pin.port, button_pin.pin);
+
+        fsmEvt = (finalBtnLvl == HIGH) ? BTN_PRESSED : BTN_RELEASED;
+
+        debouncetimer = 0;
+        btn_event = BTN_NONE;
+    }
+
+    return fsmEvt;
+}
+
+
+void LedOn(void)
+{
+    Gpio_SetLevel(led_pin.port,led_pin.pin,HIGH);
+
+}
+
+void LedOff(void)
+{
+    Gpio_SetLevel(led_pin.port,led_pin.pin,LOW);
+
+}
+
+led_states_t Fsm_Run(btn_evt_t evt , led_states_t currentstate)
+{
+    led_states_t nextState = currentstate;
+    for(uint32_t i=0;i<FS_CNT;i++)
+    {
+        if(evt==myLedSm[i].evt && currentstate == myLedSm[i].current_state)   /* Match Found */
+        {
+            myLedSm[i].Dispatch();
+            nextState = myLedSm[i].next_state;
+            break;
+        }
+    }
+
+    return nextState;
+
+}
+
+void LedBtnFsm_Run(void)
+{
+  finalBtnEvt = BTN_NONE;
+  finalBtnEvt = handledebouncecrudeway();
+        
+  led_state_main = Fsm_Run(finalBtnEvt,led_state_main);
+}
+
+
+
+
+void servoanglerxed(uint8_t *param)
+{
+  uint8_t str[256];
+  float anglef = 0.0f;
+  float dutycycle = 0.0f;
+  anglef = basic_atof((const char *)param);
+  TurnServo(anglef,2);
+  printdebugstring("Updated Servo\r\n");
+}
+
+void servofeedback(uint8_t *param)
+{
+
+    dc = (uint8_t)(dutysaved*100.0f);
+    //printdebugstring("Servo Feedback Rxed\r\n");
+    Uart_ll_SendByte(dc+0x30);
+    printdebugstring(" %duty cycle\r\n");
+
+}
+
+
+void setledon(uint8_t *param)
+{
+  LedOn();
+}
+
+void setledoff(uint8_t *param)
+{
+  LedOff();
+}
+
 void UartFsm_Run(void)
 {
     uint8_t servorx[8];
@@ -324,11 +310,9 @@ void UartFsm_Run(void)
     {
       if(dutysaved!=dutycurrent)
       {
-          dutysaved = dutycurrent;
+        dutysaved = dutycurrent;
       }
     }
-
-
     if(err_st==error_ok)
     {
       str_allsmallcase(readmymessage);
@@ -375,4 +359,23 @@ void UartFsm_Run(void)
       
     }
 
+}
+
+
+void __isr_exti15_10(void)
+{
+    
+    btn_level = Gpio_GetPin(button_pin.port,button_pin.pin);
+
+    if(btn_level == HIGH)
+    {
+        btn_event = BTN_PRESSED;
+    }
+    else if(btn_level==LOW)
+    {
+        btn_event = BTN_RELEASED;
+    }
+
+    ClearPendingInterrupt(button_pin.pin);
+   
 }
